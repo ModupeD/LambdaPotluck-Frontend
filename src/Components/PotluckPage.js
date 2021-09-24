8c81788733d38875b05292efc152e47a557fd75f
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-09-24 13:40:34 +0100

    added

--- src/Components/PotluckPage.js
+++ src/Components/PotluckPage.js
@@ -1,49 +1,47 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ListPotlucks from "./ListPotluck";
 import CreatePotluck from "./CreatePotlucks";
-import {listPotlucks} from "../Backend/api";
-import img from '../image/background.jpg'
-import {Switch,Route} from 'react-router-dom';
+import { listPotlucks } from "../Backend/api";
+import img from "../image/Yellow and Black Photo Food YouTube Channel Art.png";
+import { Switch, Route } from "react-router-dom";
 import PotluckDetails from "./PotluckDetails";
 import styled from "styled-components";
 
 const PotluckPage = () => {
+  const [potlucks, setPotlucks] = useState([]);
 
-    const [potlucks, setPotlucks] = useState([]);
+  const loadPotlucks = () => {
+    const result = listPotlucks();
+    setPotlucks(result);
+  };
 
-    const loadPotlucks = () => {
-        const result = listPotlucks();
-        setPotlucks(result);
-    }
-
-    const StyledForm= styled.div`
-       /* display: flex; */
-       
-    `
-    // const Background= styled.div`
-    //     background-image: url(${img});
-    //     height: 100%;
-    //     background-position: center;
-    //     background-size: cover;
-    // `
-    useEffect(()=>{
-        loadPotlucks();
-    }, []);
-    return (
-    // <Background>
-        <Switch>
-            <Route path="/" exact>
-               <StyledForm>
-                <CreatePotluck handleCreate={loadPotlucks}/>
-               </StyledForm>
-                <ListPotlucks potlucks={potlucks}/>
-            </Route>
-            <Route path="/potluck/:id" exact>
-                <PotluckDetails handleModify={loadPotlucks}/>
-            </Route>
-        </Switch>
-    /* </Background> */
-    );
+  const StyledForm = styled.div`
+    /* display: flex; */
+  `;
+  const Background = styled.div`
+    background-image: url(${img});
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+  `;
+  useEffect(() => {
+    loadPotlucks();
+  }, []);
+  return (
+    <Background>
+      <Switch>
+        <Route path="/" exact>
+          <StyledForm>
+            <CreatePotluck handleCreate={loadPotlucks} />
+          </StyledForm>
+          <ListPotlucks potlucks={potlucks} />
+        </Route>
+        <Route path="/potluck/:id" exact>
+          <PotluckDetails handleModify={loadPotlucks} />
+        </Route>
+      </Switch>
+    </Background>
+  );
 };
 
 export default PotluckPage;
